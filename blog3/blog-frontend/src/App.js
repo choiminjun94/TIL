bc5f98bd65bf89a72a7894e72c8d479eda7b7832
choiminjun <1020alswns@gmail.com>
2020-07-03 17:58:33 +0900

    AuthForm

---
+++ blog3/blog-frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import PostListPage from './page/PostListPage';
+import LoginPage from './page/LoginPage';
+import RegisterPage from './page/RegisterPage';
+import WritePage from './page/WritePage';
+import PostPage from './page/PostPage'
+
+function App() {
+  return (
+    <>
+      <Route component = {PostListPage} path={['/@username', '/']} exact />
+      <Route component = {LoginPage} path ="/login"/>
+      <Route component = {RegisterPage} path = "/register" />
+      <Route component = {WritePage} path ="/write" />
+      <Route component = {PostPage} path ="/@:username/:postId"/>
+    </>
+  );
+}
+
+export default App;
